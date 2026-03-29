7f54a2909d59f93b7691ceb27b7f9bb6982f0014
Rakesh Perala <raakeshperala16@gmail.com>
2026-03-29 16:16:36 +0530

    Update main.tf

--- EKS_TERRAFORM/main.tf
+++ EKS_TERRAFORM/main.tf
@@ -1,18 +1,19 @@
 
-# -----------------------------
-# Get Default VPC
-# -----------------------------
+
+############################################################
+# Get default VPC
+############################################################
 data "aws_vpc" "default" {
   default = true
 }
 
-# -----------------------------
-# Private Subnets (3 AZs)
-# -----------------------------
+############################################################
+# Create Private Subnets
+############################################################
 resource "aws_subnet" "private_subnet_1" {
-  vpc_id            = data.aws_vpc.default.id
-  cidr_block        = "172.31.100.0/24"
-  availability_zone = "ap-south-2a"
+  vpc_id                  = data.aws_vpc.default.id
+  cidr_block              = "172.31.100.0/24"
+  availability_zone       = "ap-south-2a"
   map_public_ip_on_launch = false
 
   tags = {
@@ -23,9 +24,9 @@ resource "aws_subnet" "private_subnet_1" {
 }
 
 resource "aws_subnet" "private_subnet_2" {
-  vpc_id            = data.aws_vpc.default.id
-  cidr_block        = "172.31.101.0/24"
-  availability_zone = "ap-south-2b"
+  vpc_id                  = data.aws_vpc.default.id
+  cidr_block              = "172.31.101.0/24"
+  availability_zone       = "ap-south-2b"
   map_public_ip_on_launch = false
 
   tags = {
@@ -36,9 +37,9 @@ resource "aws_subnet" "private_subnet_2" {
 }
 
 resource "aws_subnet" "private_subnet_3" {
-  vpc_id            = data.aws_vpc.default.id
-  cidr_block        = "172.31.102.0/24"
-  availability_zone = "ap-south-2c"
+  vpc_id                  = data.aws_vpc.default.id
+  cidr_block              = "172.31.102.0/24"
+  availability_zone       = "ap-south-2c"
   map_public_ip_on_launch = false
 
   tags = {
@@ -48,37 +49,90 @@ resource "aws_subnet" "private_subnet_3" {
   }
 }
 
-# -----------------------------
-# IAM Role for EKS Admin
-# -----------------------------
+############################################################
+# IAM Role for EKS Cluster
+############################################################
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "eks-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+############################################################
+# IAM Role for Fargate Pod Execution
+############################################################
+resource "aws_iam_role" "fargate_pod_execution_role" {
+  name = "eks-fargate-pod-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "eks-fargate-pods.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "fargate_policy" {
+  role       = aws_iam_role.fargate_pod_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSFargatePodExecutionRolePolicy"
+}
+
+############################################################
+# IAM Role for Admin Access
+############################################################
 resource "aws_iam_role" "eks_admin_role" {
   name = "EKS-Admin-Role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Principal = {
-        Service = "eks.amazonaws.com"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        }
+        Action = "sts:AssumeRole"
       }
-      Action = "sts:AssumeRole"
-    }]
+    ]
   })
 }
 
-# Attach AdministratorAccess policy
 resource "aws_iam_role_policy_attachment" "eks_admin_attach" {
   role       = aws_iam_role.eks_admin_role.name
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
 
-# -----------------------------
-# EKS Cluster (Fargate)
-# -----------------------------
+data "aws_caller_identity" "current" {}
+
+############################################################
+# EKS Cluster
+############################################################
 resource "aws_eks_cluster" "eks_cluster" {
   name     = "devops-eks-cluster"
-  role_arn = aws_iam_role.eks_admin_role.arn
-  version  = "1.27"
+  version  = "1.26"
+  role_arn = aws_iam_role.eks_cluster_role.arn
 
   vpc_config {
     subnet_ids = [
@@ -86,26 +140,19 @@ resource "aws_eks_cluster" "eks_cluster" {
       aws_subnet.private_subnet_2.id,
       aws_subnet.private_subnet_3.id
     ]
-    endpoint_private_access = true
-    endpoint_public_access  = true
   }
 
-  tags = {
-    Name    = "devops-eks-cluster"
-    Owner   = "DevOpsFactory"
-    Project = "DevOps-EKS"
-  }
+  enabled_cluster_log_types = ["api", "audit", "authenticator"]
 }
 
-# -----------------------------
+############################################################
 # Fargate Profile
-# -----------------------------
+############################################################
 resource "aws_eks_fargate_profile" "fargate_profile" {
   cluster_name           = aws_eks_cluster.eks_cluster.name
   fargate_profile_name   = "devops-fargate-profile"
-  pod_execution_role_arn = aws_iam_role.eks_admin_role.arn
-
-  subnet_ids = [
+  pod_execution_role_arn = aws_iam_role.fargate_pod_execution_role.arn
+  subnet_ids             = [
     aws_subnet.private_subnet_1.id,
     aws_subnet.private_subnet_2.id,
     aws_subnet.private_subnet_3.id
@@ -113,15 +160,5 @@ resource "aws_eks_fargate_profile" "fargate_profile" {
 
   selector {
     namespace = "default"
-  }
-
-  selector {
-    namespace = "kube-system"
-  }
-
-  tags = {
-    Name    = "devops-fargate-profile"
-    Owner   = "DevOpsFactory"
-    Project = "DevOps-EKS"
   }
 }
